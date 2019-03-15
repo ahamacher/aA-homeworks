9173034828c1112e1077db274a11fdb35f7f92d2
Andrew <andrew.hamacher@gmail.com>
2019-03-14 21:20:12 -0700

    Set Stack class methods

--- W1D5/Stack.rb
+++ W1D5/Stack.rb
@@ -1,19 +1,22 @@
 class Stack
+  attr_accessor :order
 
-  def initialize
-    # create ivar to store stack here!
+  def initialize(order)
+    @order = order
   end
 
   def push(el)
-    # adds an element to the stack
+    order << el
   end
 
   def pop
     # removes one element from the stack
+    self.pop
   end
 
   def peek
     # returns, but doesn't remove, the top element in the stack
+    order.last
   end
 
 end
