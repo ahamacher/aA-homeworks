25e61bf898e2f15de8dca9a8f7b6b5c1e0353390
Andrew <andrew.hamacher@gmail.com>
2019-03-14 21:26:26 -0700

    Add enqueue, dequeue, peek

--- W1D5/queue.rb
+++ W1D5/queue.rb
@@ -1,5 +1,20 @@
 class Queue
+  attr_accessor :line
 
+  def initialize(array)
+    @line = array
+  end
 
+  def enqueue(el)
+    line << el
+  end
+
+  def dequeue
+    line.shift
+  end
+
+  def peek
+    line.first
+  end
 
 end
